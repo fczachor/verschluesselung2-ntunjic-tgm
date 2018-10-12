b4388966cf30f58b6ffdbb2399cc5af79c768e49
Niko <Niko@Tunjic-Notebook>
2018-10-12 12:55:09 +0200

    test commit by niko

--- src/example/ExampleClass2.java
+++ src/example/ExampleClass2.java
@@ -3,10 +3,10 @@ package example;
 public class ExampleClass2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!!!");
-		System.out.println("Hello world!!!");
-		System.out.println("Hello world!!!");
-		System.out.println("Hello world!!!");
+		System.out.println("Hello world!!! by niko");
+		System.out.println("Hello world!!! by niko");
+		System.out.println("Hello world!!! by niko");
+		System.out.println("Hello world!!! by niko");
 	}
 
 }
